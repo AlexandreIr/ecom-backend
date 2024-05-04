8f5183eeb573d4cda28d189edd184f975c2370be
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-04 20:30:54 -0300

    feat: H2 database, test profile, JPA

--- src/main/java/com/afdesign/ecom/resources/UserResources.java
+++ src/main/java/com/afdesign/ecom/resources/UserResources.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.afdesign.ecom.entities.User;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value="/book")
 public class UserResources {
 	
 	@GetMapping
@@ -19,8 +19,10 @@ public class UserResources {
 		List<User> list = new ArrayList<>();
 		User u = new User(1L, "Alexandre", "alex.silva250@hotmail.com", "+55119999999", "123456");
 		User u2 = new User(2L, "Maria", "maria888@gmail.com", "+55118888888", "654321");
+		User u3 = new User(3L, "Adailde", "silvaadailde@gmail.com", "+551177777777", "556124");
 		list.add(u);
 		list.add(u2);
+		list.add(u3);
 		return ResponseEntity.ok().body(list);
 	}
 }
