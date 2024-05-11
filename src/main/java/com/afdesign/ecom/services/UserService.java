0f4dd4ab7da994c6a4211f3d5a1c4444bbdd46ac
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-10 22:26:13 -0300

    feat: User update method implemented

--- src/main/java/com/afdesign/ecom/services/UserService.java
+++ src/main/java/com/afdesign/ecom/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		User ent = repository.getReferenceById(id);
+		updateData(ent, user);
+		return repository.save(ent);
+	}
+	
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 }
