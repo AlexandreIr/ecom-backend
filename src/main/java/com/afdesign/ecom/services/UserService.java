ca57e2a9a35ebd9c97fc23a016100d1e89c35ab6
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-11 22:59:23 -0300

    feat: exception handling -findById

--- src/main/java/com/afdesign/ecom/services/UserService.java
+++ src/main/java/com/afdesign/ecom/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.afdesign.ecom.entities.User;
 import com.afdesign.ecom.repositories.UserRepository;
+import com.afdesign.ecom.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
